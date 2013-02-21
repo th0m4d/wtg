991fe9ca71708b08e5b234c3ce49f23cd33746e3
Thomas Pieronczyk <thomas.pieronczyk@moid.de>
2013-02-21 15:47:37 +0100

    Add function to create bag of histograms for each dictionary.

---
+++ src/create_histograms_from_gtzan.m
@@ -0,0 +1,23 @@
+function create_histograms_from_gtzan()
+
+folders = {'blues'; 'classical'; 'country'; 'disco'; 'hiphop'; 'jazz'; 'metal'; 'pop'; 'reggae'; 'rock'};
+
+savePath = './data/histograms/';
+if (exist(savePath, 'dir') == 0)
+    mkdir(savePath);
+end
+  
+for i=1:10
+    folderName = char(folders(i));
+    path = strcat('data/dictionaries/',folderName,'_data.mat');
+    % Read in the dictionaries
+    data = load(path);
+    encoding = data.A;
+
+    H = get_bag_of_histograms(encoding, 22050, 1024, 5);
+    
+    %write dictionary to file
+    filename = strcat(savePath, char(folders(i)), '_data.mat');
+    save(filename, 'H');
+            
+end
