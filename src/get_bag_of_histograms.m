b373768f74171fca5e5a556d7356de2c0393f6d5
Thomas Pieronczyk <thomas.pieronczyk@moid.de>
2013-02-21 13:58:19 +0100

    Take the overlapping from the song segmentation into account.

--- src/get_bag_of_histograms.m
+++ src/get_bag_of_histograms.m
@@ -8,16 +8,19 @@
 function B = get_bag_of_histograms(A, bitrate, windowSize, textureWindow)
 
 % number of measurements in texture window = textureWindow (stftWindowSize / bitrate)
-cols = floor(textureWindow / (windowSize / bitrate));
+cols_single = floor(textureWindow / (windowSize / bitrate));
+
+cols = (cols_single * 2) - 1;
+
 nx = size(A,2);
 
 ncol = fix(nx/cols);
 
 X = [];
 
-for i = 1:(ncol - 1)
-    startCols = cols * i;
-    endCols = cols*(i+1);
+for i = 0:(ncol - 1)
+    startCols = (cols * i)+1;
+    endCols = (cols*(i+1))+1;
     aCol = A(:,(startCols:endCols));
     %sum over the columns of aCol
     colSum = sum(aCol, 2);
