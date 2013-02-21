f67501e18322f9709eb4d8cfe0c980a31b2cb8b2
Thomas Pieronczyk <thomas.pieronczyk@moid.de>
2013-02-21 15:18:29 +0100

    Change the dictionary creation function to train dictionaries for all genres.

--- src/create_dict_from_gtzan.m
+++ src/create_dict_from_gtzan.m
@@ -6,13 +6,21 @@ savePath = './data/dictionaries/';
 if (exist(savePath, 'dir') == 0)
     mkdir(savePath);
 end
+  
+for i=1:10
+    folderName = char(folders(i));
+    path = strcat('data/spectrograms/',folderName,'_data.mat');
+    % Read in the spectrogram
+    spectrogram = load(path);
+    spectrogram = spectrogram.dat;
 
-%%train blues dictionary
-spectrogram = load('data/spectrograms/blues_data.mat');
-spectrogram = spectrogram.dat;
-[ D, A ] = train_dictionary_ksvd(spectrogram, size(spectrogram,2), 3, 10);
+    disp(strcat('Training dictionary for genre: ', folderName));
+    
+    [ D, A ] = train_dictionary_ksvd(spectrogram, size(spectrogram,2)/10, 3, 10);
 
- %write dictionary to file
- filename = strcat(savePath, char(folders(1)), '_data');
- save(filename, 'D');
- save(filename, 'A','-append');
+    %write dictionary to file
+    filename = strcat(savePath, char(folders(i)), '_data.mat');
+    save(filename, 'D');
+    save(filename, 'A','-append');
+            
+end
