83a85e120e65230213ced859826dec5aedfa56b5
Miguel Cabrera <mfcabrera@gmail.com>
2013-02-22 12:54:16 +0100

    the training to look for the best parameter using 10-fold cross validation and varying C from 1 to 20

--- src/boh_svm_train.m
+++ src/boh_svm_train.m
@@ -12,7 +12,26 @@ addpath  ./lib/fast-additive-svms/
 
 % model = svmtrain(histograms,labels);
 % default C-SVM model with C = 1 and 10-fold cross validation
-svmmodel = svmtrain(labels,histograms,'-t 5 -b 1 -v 10');
+%svmmodel = svmtrain(labels,histograms,'-t 5 -b 1 -v 10');
+
+C = 1;
+acc = 0;
+
+
+printf('Training model using 10-fold cross validation from C= 1 to 20...');
+for i = 1:1:20,
+    newacc =   svmtrain(labels,histograms,sprintf('-t 5 -b 1 -v 10 -c %i ',i));
+    if newacc > acc
+        acc = newacc;
+        C = 1;
+    end
+end
+printf('done\n');
+
+printf('Best parameter C=%i with an accuract of %f\n',C,i);
+printf('Retraining...');
+    svnmodel =   svmtrain(labels,histograms,sprintf('-t 5 -b 1 -c %i ',C));
+printf('DONE\n');
 
 end
 
