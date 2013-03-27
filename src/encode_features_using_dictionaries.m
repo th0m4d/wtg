fa515974c6467ecf8db4cf84e52ca963a91b7f08
Miguel Cabrera <mfcabrera@gmail.com>
2013-03-27 07:36:36 -0500

    forgot to add encode_features_using_dictionaries

--- src/encode_features_using_dictionaries.m
+++ src/encode_features_using_dictionaries.m
@@ -5,9 +5,22 @@ addpath ./lib/ompbox10/
 
 folders = {'blues'; 'classical'; 'country'; 'disco'; 'hiphop'; 'jazz'; 'metal'; 'pop'; 'reggae'; 'rock'};
 
-savePath = './data/sparserep/';
-if (exist(savePath, 'dir') == 0)
-    mkdir(savePath);
+
+%TODO create a function to do this
+savePathRoot = './data/sparserep/';
+savePathTraining = './data/sparserep/training/';
+savePathTesting = './data/sparserep/testing/'
+
+if (exist(savePathRoot, 'dir') == 0)
+    mkdir(savePathRoot);
+end
+
+if (exist(savePathTraining, 'dir') == 0)
+    mkdir(savePathTraining);
+end
+
+if (exist(savePathTesting, 'dir') == 0)
+    mkdir(savePathTesting);
 end
 
 joint_D = [];
@@ -34,21 +47,41 @@ fprintf('Enconding samples using a dictionary  of %d columns \n',size(joint_D,2)
 JD_norm = normc(joint_D);
 G = JD_norm'*JD_norm;
 
-
+%Encode training (Necessary? - For the SVM I would say XD)
 for i=1:10
     folderName = char(folders(i));
-    path = strcat('data/spectrograms/',folderName,'_data.mat');
+    path = strcat('data/spectrograms/training/',folderName,'_data.mat');
     % Read in the spectrogram
     spectogram = load(path);
-    spectogram = spectogram.dat;
+    spectogram = spectogram.dat_training;
 
     disp(strcat('Encoding genre:', folderName));
     % We enconde using OMP per genre
     gamma = omp(JD_norm,spectogram,G, sparcity);
     
-    %write dictionary to file
-    filename = strcat(savePath, char(folders(i)), '_data.mat');
+    %write representation to file
+    filename = strcat(savePathTraining, char(folders(i)), '_data.mat');
     save(filename, 'gamma');
 
             
 end
+
+%Encodeing testing
+for i=1:10
+    folderName = char(folders(i));
+    path = strcat('data/spectrograms/testing/',folderName,'_data.mat');
+    % Read in the spectrogram
+    spectogram = load(path);
+    spectogram = spectogram.dat_testing;
+
+    disp(strcat('Encoding genre:', folderName));
+    % We enconde using OMP per genre
+    gamma = omp(JD_norm,spectogram,G, sparcity);
+    
+    %write representation to file
+    filename = strcat(savePathTesting, char(folders(i)), '_data.mat');
+    save(filename, 'gamma');
+
+            
+end
+
