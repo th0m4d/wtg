50d736cc5aadf6f6c76bc9956bd3736bda695fff
Thomas Pieronczyk <thomas.pieronczyk@moid.de>
2013-07-04 16:17:04 +0200

    Parallelize creation of dictionaries.

--- src/create_dict_from_gtzan.m
+++ src/create_dict_from_gtzan.m
@@ -3,34 +3,20 @@ function create_dict_from_gtzan(genre_dict_size, num_iterations, target_sparcity
 % genre_dict_size is the size of the dictionary for each genre.
 % the dictionaries are written in data/dictionaries
 
-% K-SVD implementation from Ron Rubinstein
-addpath ./lib/ompbox10/
-addpath ./lib/ksvdbox13/
+num_genres = size(folders,2);
 
-savePath = './data/dictionaries/';
-if (exist(savePath, 'dir') == 0)
-    mkdir(savePath);
-end
-
-num_genres = size(folders,1);
+%create job
+profileName = parallel.defaultClusterProfile();
+cluster = parcluster(profileName);
+job = createJob(cluster)
 
 for i=1:num_genres
-    folderName = char(folders(i));
-    path = strcat('data/', feature_extraction_method,'s', '/training/',folderName,'_data.mat');
-    % Read in the spectrogram
-    feature = load(path);
-    feature = feature.dat_training;
-
-    disp(strcat('Training dictionary for genre: ', folderName));
-    disp(strcat('Processing file: ', path));
-    
-    % there was an error here when dividing by then. A inner loop is
-    % missing.
-    [ D, A ] = train_dictionary_ksvdbox(genre_dict_size,feature, size(feature,2)/100, target_sparcity, num_iterations, varargin);
-
-    %write dictionary to file
-    filename = strcat(savePath, char(folders(i)), '_data.mat');
-    save(filename, 'D');
-    save(filename, 'A','-append');
-            
+    createTask(job, @create_dict_for_genre, 0, {char(folders(i)),genre_dict_size, num_iterations, target_sparcity, feature_extraction_method, varargin});
 end
+
+fprintf('Starting job to create dictionaries\n');
+%job.Tasks
+submit(job)
+wait(job)
+delete(job)
+fprintf('Job finished.\n');
