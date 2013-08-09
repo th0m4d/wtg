1ae1c899ba85518a90d47358ec47e73150087bb0
Thomas Pieronczyk <thomas.pieronczyk@moid.de>
2013-08-09 17:02:53 +0200

    Modify feature extraction to support mp3 files.

--- src/wtg_gui.m
+++ src/wtg_gui.m
@@ -60,6 +60,15 @@ guidata(hObject, handles);
 
 % UIWAIT makes wtg_gui wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
+%Add library paths
+addpath ./src/lib/cqt_toolbox
+addpath ./src/lib/k-svd
+addpath ./src/lib/fast-additive-svms
+addpath ./src/lib/fast-additive-svms/libsvm-mat-3.0-1
+% K-SVD implementation from Ron Rubinstein
+addpath ./src/lib/ompbox10
+addpath ./src/lib/ksvdbox13
+
 
 % --- Outputs from this function are returned to the command line.
 function varargout = wtg_gui_OutputFcn(hObject, eventdata, handles) 
@@ -147,18 +156,33 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
 end
 
 function openFile(handles)
-    filename = uigetfile;
-    set(handles.file_path_edit_text,'string', filename);
+    fileSpec = {'*.mp3;*.au'}
+    [baseName, folder] = uigetfile(fileSpec,'Select audio file to open.');
+    filename = fullfile(folder, baseName)
+    if filename ~= 0
+        set(handles.file_path_edit_text,'string', filename);
+    end
     
 function classify(handles)
+    filePath = get(handles.file_path_edit_text,'string');
+    features = getFeatures(filePath, handles)
+    histogram = getHistogram(features)
+    
+function [histogram] = getHistogram(features)
+    histogram
+
+
+function [features] = getFeatures(filePath, handles)
     audio_feature = getCurrentPopupString(handles.audio_feature_popup)
-    filepath = get(handles.file_path_edit_text,'string');
     features = [];
     if strcmp(audio_feature, 'Spectrogram') == 1;
-        features = get_spec_from_audio(filepath);
+        features = get_spec_from_audio(filePath,'norm');
     elseif strcmp(audio_feature, 'CQT') == 1;
-        features = get_cqt_from_audio(filepath);
+        features = get_cqt_from_audio(filePath);
+    else
+        error('Audio feature not supported.')
     end
+    features
     
 function str = getCurrentPopupString(hh)
     %# getCurrentPopupString returns the currently selected string in the popupmenu with handle hh
